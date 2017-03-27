f3d1beabae67ce993a9570dbbb9c091c16d00931
Kracav4ik <dikama2013@yandex.ru>
2017-03-27 22:57:48 +0300

    Fixed double-changed color bug

--- src/widgets/ColorChooserWidget.cpp
+++ src/widgets/ColorChooserWidget.cpp
@@ -4,83 +4,81 @@ ColorChooserWidget::ColorChooserWidget(QWidget* parent): QDockWidget(parent) {
     setupUi(this);
     selectColor(QColor(255, 0, 255));
 
-    connect(editR, SIGNAL(textEdited(const QString&)), SLOT(updateColor(const QString&)));
-    connect(editG, SIGNAL(textEdited(const QString&)), SLOT(updateColor(const QString&)));
-    connect(editB, SIGNAL(textEdited(const QString&)), SLOT(updateColor(const QString&)));
-    connect(editH, SIGNAL(textEdited(const QString&)), SLOT(updateColor(const QString&)));
-    connect(editS, SIGNAL(textEdited(const QString&)), SLOT(updateColor(const QString&)));
-    connect(editL, SIGNAL(textEdited(const QString&)), SLOT(updateColor(const QString&)));
+    connect(editR, SIGNAL(textChanged(const QString&)), SLOT(updateColor()));
+    connect(editG, SIGNAL(textChanged(const QString&)), SLOT(updateColor()));
+    connect(editB, SIGNAL(textChanged(const QString&)), SLOT(updateColor()));
+    connect(editH, SIGNAL(textChanged(const QString&)), SLOT(updateColor()));
+    connect(editS, SIGNAL(textChanged(const QString&)), SLOT(updateColor()));
+    connect(editL, SIGNAL(textChanged(const QString&)), SLOT(updateColor()));
+}
+
+template<typename T>
+QString str(const T& t) {
+    return QString("%1").arg(t);
+}
+
+void printColor(const QColor& c) {
+    int r, g, b, h, s, l;
+    c.getRgb(&r, &g, &b);
+    c.getHsl(&h, &s, &l);
+    printf("r = %d, g = %d, b = %d, h = %d, s = %d, l = %d\n", r, g, b, h, s, l);
 }
 
 void ColorChooserWidget::selectColor(const QColor& c) {
+    isUpdating = true;
+
     color = c;
 
     QPalette p = selectedColor->palette();
     p.setColor(QPalette::Background, c);
     selectedColor->setPalette(p);
 
-    editR->setText(QString("%1").arg(c.red()));
-    editG->setText(QString("%1").arg(c.green()));
-    editB->setText(QString("%1").arg(c.blue()));
+    editR->setText(str(c.red()));
+    editG->setText(str(c.green()));
+    editB->setText(str(c.blue()));
     if (c.hue() != -1) {
-        editH->setText(QString("%1").arg(c.hue()));
-        editS->setText(QString("%1").arg(c.saturation()));
+        editH->setText(str(c.hslHue()));
+        editS->setText(str(c.hslSaturation()));
     }
-    editL->setText(QString("%1").arg(c.lightness()));
+    editL->setText(str(c.lightness()));
     sliderL->setValue(c.lightness());
+
+    isUpdating = false;
+}
+
+#define GET_FROM_EDIT(VAR_NAME, MIN_VAR, MAX_VAR)                    \
+int VAR_NAME;                                                        \
+{                                                                    \
+    bool ok;                                                         \
+    VAR_NAME = edit##VAR_NAME->text().toInt(&ok);                    \
+    if (!ok || VAR_NAME < (MIN_VAR) || VAR_NAME > (MAX_VAR)) {       \
+        return;                                                      \
+    }                                                                \
 }
 
 
-
-void ColorChooserWidget::updateColor(const QString& s) {
-    QObject* senderObject = sender();
-    bool ok = false;
-    int value = s.toInt(&ok);
-    if (!ok) {
+void ColorChooserWidget::updateColor() {
+    if (isUpdating) {
         return;
     }
+    QObject* senderObject = sender();
+    QColor c;
 
-    QColor c = color;
-    if (senderObject == editR){
-        if (value < 0 || value > 255){
-            return;
-        }
-        c.setRed(value);
-    } else if(senderObject == editG){
-        if (value < 0 || value > 255){
-            return;
-        }
-        c.setGreen(value);
-    } else if(senderObject == editB){
-        if (value < 0 || value > 255){
-            return;
-        }
-        c.setBlue(value);
-    } else if(senderObject == editH){
-        if (value < 0 || value > 359){
-            return;
-        }
-        c.setHsl(value, c.saturation(), c.lightness());
-    } else if(senderObject == editS){
-        if (value < 0 || value > 255){
-            return;
-        }
-        c.setHsl(c.hue(), value, c.lightness());
-    } else if(senderObject == editL){
-        if (value < 0 || value > 255){
-            return;
-        }
-        c.setHsl(c.hue(), c.saturation(), value);
+    if (senderObject == editR || senderObject == editG || senderObject == editB){
+        GET_FROM_EDIT(R, 0, 255);
+        GET_FROM_EDIT(G, 0, 255);
+        GET_FROM_EDIT(B, 0, 255);
+        c.setRgb(R, G, B);
+    } else if (senderObject == editH || senderObject == editS || senderObject == editL){
+        GET_FROM_EDIT(H, 0, 359);
+        GET_FROM_EDIT(S, 0, 255);
+        GET_FROM_EDIT(L, 0, 255);
+        c.setHsl(H, S, L);
     }
-    
+
     selectColor(c);
 }
 
 void ColorChooserWidget::on_sliderL_valueChanged(int value) {
-    QColor c = color;
-    if (value < 0 || value > 255){
-        return;
-    }
-    c.setHsl(c.hue(), c.saturation(), value);
-    selectColor(c);
+    editL->setText(str(value));
 }
--- src/widgets/ColorChooserWidget.h
+++ src/widgets/ColorChooserWidget.h
@@ -7,11 +7,12 @@
 class ColorChooserWidget : public QDockWidget, private Ui::ColorChooserWidget {
 Q_OBJECT
 private slots:
-    void updateColor(const QString& s);
+    void updateColor();
     void on_sliderL_valueChanged(int value);
 
 private:
     QColor color;
+    bool isUpdating = false;
     void selectColor(const QColor& c);
 
 public:
